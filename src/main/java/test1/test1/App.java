3d11ae8b3f4689e31211c0ad5b4b275d2d3d61ff
2208021 <2208021@2208021NB50.iead.local>
2022-08-16 14:30:41 +0800

    update

--- src/main/java/test1/test1/App.java
+++ src/main/java/test1/test1/App.java
@@ -1,10 +1,10 @@
 package test1.test1;
 
-import com.google.gson.Gson;
+
 
 public class App {
   public static void main(String[] args) {
-	  Gson gson = new Gson();
-    System.out.println(gson.toJson("Hello World!"));
+	  
+    System.out.println("Hello World!");
   }
 }
